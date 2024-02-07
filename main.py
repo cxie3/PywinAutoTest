d239e5bffa7c47d2be506d0b740ae7e36f1caba5
Christina <christinaxie15@gmail.com>
2024-02-07 00:12:19 -0500

    Testing Pywinauto (open notepad and add text)

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ main_dlg = app.UntitledNotepad
 main_dlg.wait('visible')
 
 main_dlg = app.window(title='Untitled - Notepad')
-
 main_dlg.Edit.type_keys("Test pywinauto\n\t sample text^A",
                         with_spaces=True,
                         with_newlines=True,
                         pause=0.5,
                         with_tabs=True)
+
